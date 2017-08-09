5148349aef60ca9cd0e1565fe5121fe614d0e42c
Charlie Stanard <charlie@bokkagroup.com>
2017-08-09 14:14:36 -0600

    Removing example code

--- assets/src/js/helpers/helpers.js
+++
@@ -1,7 +0,0 @@
-export function exampleFunction(x) {
-    return x * x;
-}
-
-export function anotherExampleFunction(x) {
-    return x * 2;
-}
--- assets/src/js/initialize.js
+++ assets/src/js/initialize.js
@@ -1,39 +1,7 @@
 import $ from 'jQuery';
-import * as helpers from './helpers/helpers';
 
 $(document).ready(() => {
 
-    //example module usage
-    let result = helpers.exampleFunction(4);
-    console.log(result); // logs 16
-    let result2 = helpers.anotherExampleFunction(4);
-    console.log(result2); // logs 8
+    //...code
 
-    //example promise
-    let someVar = false;
-
-    let testPromise = new Promise((resolve, reject) => {
-        if (someVar) {
-            return resolve('it worked!');
-        } else {
-            return reject('it failed :(');
-        }
-    });
-
-    testPromise.then((message) => {
-        console.log(message);
-    }, (error) => {
-        console.log(error);
-    });
-
-    //example handlebars template
-    let template = require('./templates/example.handlebars');
-    let exampleTemplate = document.createElement('div');
-
-    exampleTemplate.innerHTML = template({
-        headline: "This is the headline",
-        author: "Charlie"
-    });
-
-    document.body.appendChild(exampleTemplate);
 });
