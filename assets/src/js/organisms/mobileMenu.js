d4aa4ace6449ced68b18664f75ce562b6f86659c
Charlie Stanard <charlie@bokkagroup.com>
2018-03-12 17:00:14 -0600

    WIP

--- assets/src/js/organisms/mobileMenu.js
+++ assets/src/js/organisms/mobileMenu.js
@@ -1,44 +1,68 @@
 import $        from 'jquery';
+import _        from 'lodash';
 
 /**
- * Mobile navigation
- * @type {mobileMenu}
- */
+* Mobile navigation
+* @type {mobileMenu}
+*/
 export default class mobileMenu {
 
-	/**
-	* Initialize mobile menu
-	* @param  {string} selector Element wrapper selector
-	* @param  {array}  options  Array of options that override defaults
-	*/
-	constructor(selector, options) {
-		this.options = Object.assign({
-			toggleSelector: '.menu-toggle',
-			menuSelector: '.menu'
-		}, options);
+    /**
+    * Initialize mobile menu
+    * @param  {string} selector Element wrapper selector
+    * @param  {array}  options  Array of options that override defaults
+    */
+    constructor(selector, options) {
+        this.options = Object.assign({
+            toggleSelector: '.menu-toggle',
+            menuSelector: '.menu'
+        }, options);
 
-		this.$selector = $(selector);
+        this.$selector = $(selector);
 
-		// Check visibility of menu toggle before attaching event listeners
-		if ($(this.options.toggleSelector).is(':visible')) {
-			this.attachEventHandlers();
-		}
-	}
+        this.toggleEventHandlers();
 
-	attachEventHandlers() {
-		const self = this;
+        $(window).on('resize', _.debounce(this.toggleEventHandlers.bind(this), 300));
+    }
 
-		// Toggle mobile menu display
-		$(self.options.toggleSelector).on('click', function (event) {
-			event.preventDefault();
-			self.$selector.find(self.options.menuSelector).toggleClass('menu-open');
-		});
+    /**
+     * Attach/destroy event handlers as necessary
+     */
+    toggleEventHandlers() {
+        if (this.isMobile()) {
+            // if they haven't been attached yet, attach (desktop --> mobile)
+            // if event handlers have already been attached, don't do anything (mobile --> mobile)
+            this.attachEventHandlers();
+        } else {
+            // if event handlers have already been attached, destroy (mobile --> desktop)
+        }
+    }
 
-		// Toggle submenu display
-		$(self.options.menuSelector).find('.has-submenu > a').on('click', function (event) {
-			event.preventDefault();
-			$(this).next('.submenu').toggleClass('menu-open');
-			$(this).toggleClass('submenu-open');
-		});
-	}
+    /**
+     * Attach event listeners for menu toggles
+     */
+    attachEventHandlers() {
+        const self = this;
+
+        // Toggle mobile menu display
+        $(self.options.toggleSelector).on('click', function (event) {
+            event.preventDefault();
+            self.$selector.find(self.options.menuSelector).toggleClass('menu-open');
+        });
+
+        // Toggle submenu display
+        $(self.options.menuSelector).find('.has-submenu > a').on('click', function (event) {
+            event.preventDefault();
+            $(this).next('.submenu').toggleClass('menu-open');
+            $(this).toggleClass('submenu-open');
+        });
+    }
+
+    /**
+    * Check visibility of menu toggle before attaching event listeners
+    * @return {Boolean}
+    */
+    isMobile() {
+        return $(this.options.toggleSelector).is(':visible') ? true : false;
+    }
 }
