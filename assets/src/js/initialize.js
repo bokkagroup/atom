c39d5c22d3391b4ed4993e93fc7fcce518ef72ec
Charlie Stanard <louis.stanard@gmail.com>
2018-03-14 09:27:09 -0600

    Basic mobile menu
    * Initial styles and functionality for default mobile menu
    
    * Moving has-submenu class from a to li tag in menu
    
    * Adding single-level desktop dropdown
    
    * Adding methods to add/remove event listeners on window resize
    
    * Removing classes on desktop

--- assets/src/js/initialize.js
+++ assets/src/js/initialize.js
@@ -1,13 +1,17 @@
 import $            from 'jquery';
-import tabs         from './helpers/tabs.js';
-import modals       from './helpers/modals.js';
-import sliders      from './helpers/sliders.js';
+import tabs         from './helpers/tabs';
+import modals       from './helpers/modals';
+import sliders      from './helpers/sliders';
+import mobileMenu   from './organisms/mobileMenu';
 
 $(document).ready(() => {
 
     sliders();
     modals();
     tabs();
+
+    // Initialize header mobile menu
+    let headerMenu = new mobileMenu('.organism.header');
 
     /**
      * Accordion
---
+++ assets/src/js/organisms/mobileMenu.js
@@ -0,0 +1,81 @@
+import $        from 'jquery';
+import _        from 'lodash';
+
+/**
+* Mobile navigation
+* @type {mobileMenu}
+*/
+export default class mobileMenu {
+
+    /**
+    * Initialize mobile menu
+    * @param  {string} selector Element wrapper selector
+    * @param  {array}  options  Array of options that override defaults
+    */
+    constructor(selector, options) {
+        this.options = Object.assign({
+            toggleSelector: '.menu-toggle',
+            menuSelector: '.menu'
+        }, options);
+
+        this.$selector = $(selector);
+        this.init = false;
+
+        this.toggleEventHandlers();
+
+        $(window).on('resize', _.debounce(this.toggleEventHandlers.bind(this), 300));
+    }
+
+    /**
+     * Attach/destroy event handlers as necessary
+     */
+    toggleEventHandlers() {
+        if (this.isMobile()) {
+            if (!this.init) {
+                this.attachEventHandlers();
+                this.init = true;
+            }
+        } else {
+            this.destroyEventHandlers();
+            this.init = false;
+        }
+    }
+
+    /**
+     * Attach event listeners
+     */
+    attachEventHandlers() {
+        const self = this;
+
+        // Toggle mobile menu display
+        $(self.options.toggleSelector).on('click', function (event) {
+            event.preventDefault();
+            self.$selector.find(self.options.menuSelector).toggleClass('menu-open');
+        });
+
+        // Toggle submenu display
+        $(self.options.menuSelector).find('.has-submenu > a').on('click', function (event) {
+            event.preventDefault();
+            $(this).next('.submenu').toggleClass('menu-open');
+            $(this).toggleClass('submenu-open');
+        });
+    }
+
+    /**
+    * Remove event listeners
+    */
+    destroyEventHandlers() {
+        // Remove submenu display toggle
+        $(this.options.menuSelector).find('.has-submenu > a').off('click');
+        // Remove menu-open class from any open submenus
+        $('.submenu').removeClass('menu-open');
+    }
+
+    /**
+    * Check visibility of menu toggle before attaching event listeners
+    * @return {Boolean}
+    */
+    isMobile() {
+        return $(this.options.toggleSelector).is(':visible') ? true : false;
+    }
+}
