bf760d08bb1faec2f49fb140fc65b41dad4c2b7b
Charlie Stanard <charlie@bokkagroup.com>
2018-05-07 14:35:09 -0600

    Adding header and footer views

---
+++ views/FooterView.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CatalystWP\Atom\views;
+
+class FooterView extends \CatalystWP\Nucleus\View
+{
+    public function initialize()
+    {
+        $this->template = 'layouts/footer';
+    }
+}
---
+++ views/HeaderView.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CatalystWP\Atom\views;
+
+class HeaderView extends \CatalystWP\Nucleus\View
+{
+    public function initialize()
+    {
+        $this->template = 'layouts/header';
+    }
+}
