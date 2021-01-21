82c748794430ced44e743b1e1dd5fb26cfeb7eff
Your Name <mike@bokkagroup.com>
2021-01-21 11:05:43 -0700

    Making organisms model consistent with ACF field

--- controllers/OrganismsController.php
+++ controllers/OrganismsController.php
@@ -6,7 +6,7 @@ class OrganismsController extends \CatalystWP\Nucleus\Controller
 {
     public function initialize()
     {
-        $this->model->data['organisms'] = $this->model->data;
+        $this->model->data['organism'] = $this->model->data;
         $this->view->display($this->model->data);
     }
 }
