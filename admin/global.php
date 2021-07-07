0842ab46ebb8cd53d2ab484eb0b808a6653e8020
Your Name <mike@bokkagroup.com>
2021-07-07 09:02:06 -0600

    Catalyst environment bugs

--- admin/global.php
+++ admin/global.php
@@ -8,17 +8,19 @@ function update_user_option_admin_color()
 {
     $color_scheme = 'light';
 
-    if (CATALYST_WP_ENV == 'local') {
-        $color_scheme = 'ocean';
-    }
+    if (defined("CATALYST_WP_ENV")) {
+        if (CATALYST_WP_ENV == 'local') {
+            $color_scheme = 'ocean';
+        }
 
-    if (CATALYST_WP_ENV == 'staging') {
-        $color_scheme = 'ectoplasm';
-    }
+        if (CATALYST_WP_ENV == 'staging') {
+            $color_scheme = 'ectoplasm';
+        }
 
-    if (CATALYST_WP_ENV == 'production') {
-        $color_scheme = 'sunrise';
-    }
+        if (CATALYST_WP_ENV == 'production') {
+            $color_scheme = 'sunrise';
+        }
 
-    update_user_meta(get_current_user_id(), 'admin_color', $color_scheme);
+        update_user_meta(get_current_user_id(), 'admin_color', $color_scheme);
+    }
 }
--- config.php
+++ config.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * Define theme location
- */
-define('CATALYST_WP_THEME_DIR', get_bloginfo('template_directory'));
 
 /**
  * Defines Environment Variables for theme
@@ -10,7 +6,7 @@ define('CATALYST_WP_THEME_DIR', get_bloginfo('template_directory'));
 if (!defined('CATALYST_WP_ENV') && isset($_SERVER) && $_SERVER['HTTP_HOST']) {
     $host = $_SERVER['HTTP_HOST'];
 
-    if (strpos($host, 'local') !== false || strpos($host, 'dev') !== false) {
+    if (strpos($host, 'local') !== false || strpos($host, 'dev') !== false || strpos($host, 'test') !== false) {
         define('CATALYST_WP_ENV', "local");
     } elseif (strpos($host, 'staging') !== false) {
         define('CATALYST_WP_ENV', "staging");
